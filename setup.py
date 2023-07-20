0f6218538ef26fbe1b77c21873de31569d67e30d
Andriy414414 <133632261+Andriy414414@users.noreply.github.com>
2023-07-20 22:18:55 +0300

    2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='clean_folder',
       version='01',
       description='instrument for folder cleaning',
-      url='https://github.com/Andriy414414/clean_folder.git',
+      url='https://github.com/Andriy414414/clean_folder_2.git',
       author='Andriy Kotenko',
       author_email='414andrey@gmail.com',
       entry_points={"console_scripts": ['clean_folder_2=clean_folder_2.clean_2:main']}
